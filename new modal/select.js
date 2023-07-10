46e47f7b138c8f34ce295a7ef0c09c79cc609d93
Dmitriy <93647407+1swaN@users.noreply.github.com>
2023-07-10 23:23:30 +0400

    blockquotes

--- new modal/select.js
+++
@@ -1,8 +0,0 @@
-const defaultSelect = () =>{
-    const element = document.querySelector('.language-select')
-    const choices = new Choices(element, {
-        searchEnabled: false,
-    })
-}
-
-defaultSelect()
